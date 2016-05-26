657537eb060260fb47651b5955b64ba1a0026bbc
Bob Whitelock <bob.whitelock@alces-software.com>
2016-05-26 12:10:18 +0100

    Greatly simplify tests
    Pull as much as possible out of each test case into generic methods.

--- tests/functional.rb
+++ tests/functional.rb
@@ -19,55 +19,50 @@ describe '/trigger/:script' do
   setup
 
   it 'triggers scripts correctly and returns text response' do
-    response = make_authenticated_request('printer', standard_test_json)
-    assert_equal '200', response.code
+    make_authenticated_request
 
-    response_json = JSON.parse(response.body)
+    assert_success
 
     expected_response = {"responses"=>[{"profile"=>"repo1", "contentType"=>"text/plain", "exitCode"=>0, "result"=>"-x\n--long-option\n20\n--\nfirst\nsecond argument\nHere is the stdin for the script\n"}, {"profile"=>"repo2", "contentType"=>"text/plain", "exitCode"=>1, "result"=>""}]}
     assert_equal response_json, expected_response
   end
 
   it 'returns result as json when first line is "#json"' do
-    response = make_authenticated_request('json_printer', standard_test_json)
-    assert_equal '200', response.code
+    @trigger = 'json_printer'
+    make_authenticated_request
 
-    response_json = JSON.parse(response.body)
+    assert_success
 
     expected_response = {"responses"=>[{"profile"=>"repo1", "exitCode"=>0, "contentType"=>"application/json", "result"=>{"args"=>["-x", "--long-option", "20", "--", "first", "second argument"], "stdin"=>"Here is the stdin for the script", "moreJson"=>{"foo"=>5, "bar"=>6}}}]}
     assert_equal response_json, expected_response
   end
 
   it 'validates request JSON contains needed properties' do
-    test_json = JSON.parse(standard_test_json).
+    @test_data = JSON.parse(standard_test_json).
       delete_if {|k| k == 'args'}.
       to_json
+    make_authenticated_request
 
-    response = make_authenticated_request('printer', test_json)
-    assert_equal '422', response.code
-
-    response_json = JSON.parse(response.body)
+    assert_unprocessable_entity
     assert_equal "The property '#/' did not contain a required property of 'args'",
       response_json['error']
   end
 
   it 'returns overall error if receives invalid request JSON' do
-    invalid_json = 'foo'
+    @test_data = 'foo'
+    make_authenticated_request
 
-    response = make_authenticated_request('printer', invalid_json)
-    assert_equal '422', response.code
-
-    response_json = JSON.parse(response.body)
+    assert_unprocessable_entity
     assert_equal "Received invalid request JSON", response_json['error']
   end
 
   it 'returns trigger-level error if trigger script gives error' do
-    response = make_authenticated_request('bad_interpreter', standard_test_json)
+    @trigger = 'bad_interpreter'
+    make_authenticated_request
 
-    # Overall request is successful.
-    assert_equal '200', response.code
+    # Overall request should be successful.
+    assert_success
 
-    response_json = JSON.parse(response.body)
     trigger_response = response_json['responses'].first
 
     # Only makes sense for trigger response to have these keys.
@@ -75,54 +70,55 @@ describe '/trigger/:script' do
   end
 
   it 'returns unauthorized if credentials not supplied' do
-    response = make_unauthenticated_request('printer', standard_test_json)
-    assert_equal '401', response.code
+    make_unauthenticated_request
+    assert_unauthorized
   end
 
   it 'returns unauthorized if invalid credentials supplied' do
-    response = make_unauthenticated_request_with_invalid_credentials('printer', standard_test_json)
-    assert_equal '401', response.code
+    make_request_with_invalid_credentials
+    assert_unauthorized
   end
 
   private
 
-  def make_authenticated_request(trigger, data)
-    uri = create_uri(trigger)
-    authed_request = create_request(uri, data).tap do |request|
+  def make_authenticated_request
+    @request = request.tap do |request|
       request.basic_auth 'foo', 'bar'
     end
-    make_request(authed_request, uri)
+    make_request
   end
 
-  def make_unauthenticated_request(trigger, data)
-    uri = create_uri(trigger)
-    request = create_request(uri, data)
-    make_request(request, uri)
-  end
-
-  def make_unauthenticated_request_with_invalid_credentials(trigger, data)
-    uri = create_uri(trigger)
-    authed_request = create_request(uri, data).tap do |request|
+  def make_request_with_invalid_credentials
+    @request = request.tap do |request|
       request.basic_auth 'bad_username', 'bad_password'
     end
-    make_request(authed_request, uri)
+    make_request
   end
 
-  def create_uri(trigger)
-    URI.join(TEST_URI, '/trigger/', trigger)
+  def test_data
+    @test_data ||= standard_test_json
   end
 
-  def create_request(uri, data)
-    Net::HTTP::Post.new(uri).tap do |request|
-      request.body = data
+  def uri
+    @uri ||= URI.join(TEST_URI, '/trigger/', trigger)
+  end
+
+  def trigger
+    @trigger ||= 'printer'
+  end
+
+  def request
+    @request ||= Net::HTTP::Post.new(uri).tap do |request|
+      request.body = test_data
     end
   end
 
-  def make_request(request, uri)
-    Net::HTTP.start(uri.hostname, uri.port) do |http|
+  def make_request
+    @response = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request(request)
     end
   end
+  alias make_unauthenticated_request make_request
 
   def standard_test_json
     load_test_data('standard.json')
@@ -134,5 +130,21 @@ describe '/trigger/:script' do
 
   def test_data_path
     File.join(File.dirname(__FILE__), 'test_data')
+  end
+
+  def response_json
+    JSON.parse(@response.body)
+  end
+
+  def assert_success
+    assert_equal '200', @response.code
+  end
+
+  def assert_unprocessable_entity
+    assert_equal '422', @response.code
+  end
+
+  def assert_unauthorized
+    assert_equal '401', @response.code
   end
 end
